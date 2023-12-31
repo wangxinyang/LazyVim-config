ac090da6c276421d3703fd142f406c3570fc076b
Tosei <359387050@qq.com>
2023-12-31 15:05:44 +0900

    add flutter configs

---
+++ lua/plugins/flutter.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = function()
+      require("flutter-tools").setup({
+        -- flutter_path = "/Users/tosei/workspace/flutter",
+        -- fvm = true, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
+        ui = {
+          -- the border type to use for all floating windows, the same options/formats
+          -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+          border = "rounded",
+          -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+          -- please note that this option is eventually going to be deprecated and users will need to
+          -- depend on plugins like `nvim-notify` instead.
+          notification_style = "plugin",
+        },
+        decorations = {
+          statusline = {
+            -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+            -- this will show the current version of the flutter app from the pubspec.yaml file
+            app_version = true,
+            -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+            -- this will show the currently running device if an application was started with a specific
+            -- device
+            device = true,
+          },
+        },
+        outline = {
+          open_cmd = "30vnew", -- command to use to open the outline buffer
+          auto_open = false, -- if true this will open the outline automatically when it is first populated
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          --[[ register_configurations = function(_)
+            local dap = require("dap")
+            -- dap.adapters.dart = {
+            -- 	type = "executable",
+            -- 	command = "node",
+            -- 	args = { debugger_path, "flutter" },
+            -- }
+            dap.set_log_level("TRACE")
+            dap.configurations.dart = {}
+            require("dap.ext.vscode").load_launchjs()
+          end, ]]
+        },
+        dev_log = {
+          enabled = false,
+          -- open_cmd = "tabedit", -- command to use to open the log buffer
+        },
+        lsp = {
+          color = { -- show the derived colours for dart variables
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = false, -- highlight the background
+            foreground = false, -- highlight the foreground
+            virtual_text = true, -- show the highlight using virtual text
+            virtual_text_str = "■", -- the virtual text character to highlight
+          },
+          settings = {
+            showTodos = true,
+            completeFunctionCalls = true,
+            renameFilesWithClasses = "prompt", -- "always"
+            enableSnippets = true,
+            enableSdkFormatter = true,
+          },
+        },
+      })
+    end,
+  },
+}
