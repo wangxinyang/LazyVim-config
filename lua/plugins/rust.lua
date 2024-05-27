2dc7b72be674d2ddb662fad269a2a9712726828f
wangxinyang1983@gmail.com <359387050@qq.com>
2024-05-27 13:50:01 +0900

    modify the leptos fmt config

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,7 +1,20 @@
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
+
+-- 检查是否使用 Leptos
+local function is_using_leptos()
+  local cargo_toml_path = util.path.join(vim.loop.cwd(), "Cargo.toml")
+  if vim.fn.filereadable(cargo_toml_path) == 1 then
+    for line in io.lines(cargo_toml_path) do
+      if line:match("leptos") then
+        return true
+      end
+    end
+  end
+  return false
+end
+
 return {
-  {
-    "folke/neoconf.nvim",
-  },
   {
     "mrcjkb/rustaceanvim",
     version = "^4", -- Recommended
@@ -38,6 +51,10 @@ return {
                 ["async-recursion"] = { "async_recursion" },
               },
             },
+            rustfmt = is_using_leptos() and {
+              overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
+            } or nil,
+
             -- format rust代码的时候需要注释掉下面的代码，使用leptos的时候打开
             -- rustfmt = {
             -- overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
